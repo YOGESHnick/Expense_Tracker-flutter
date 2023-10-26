867bf5e3098a4cee15a979903302d13eebe96d07
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-10-26 22:01:22 +0530

    organizing folder  structure

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,6 +1,7 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
-import 'package:expense_tracker/expenses.dart';
+import 'package:expense_tracker/widgets/expenses.dart';
 
 class Expenses extends StatefulWidget{
   const Expenses({super.key});
@@ -28,9 +29,11 @@ class _ExpensesState extends State<Expenses>{
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children:const   [
-          Text('The chart'),
-          Text('The List'),
+        children:  [
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses)
+          ),
         ],
       ),
     );
