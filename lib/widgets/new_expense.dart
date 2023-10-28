9ccce9e73971f9ff5eeee1d62d45bd467e7aa337
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-10-28 16:50:24 +0530

    added money input!

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -11,9 +11,18 @@ class NewExpense extends StatefulWidget{
 
 class _NewExpenseState extends State<NewExpense> {
 
-  var _enteredTitle = '';
-  void _saveTitleInput(String inputValue){
-    _enteredTitle=inputValue;
+  // var _enteredTitle = '';
+  // void _saveTitleInput(String inputValue){
+  //   _enteredTitle=inputValue;
+  // }
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
   }
 
   @override
@@ -23,17 +32,30 @@ class _NewExpenseState extends State<NewExpense> {
        child: Column(
         children: [
           TextField(
-            onChanged: _saveTitleInput,
+            controller: _titleController,
             maxLength: 50,
             decoration: const InputDecoration(
               label: Text('Title')
             ),
           ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: '₹ ',
+              label: Text('Amount')
+            ),
+          ),
           Row(
             children: [
-              ElevatedButton(
+              TextButton(onPressed: (){
+                Navigator.pop(context);
+              }, 
+              child: const Text('Cancel')),
+              TextButton(
                 onPressed:(){
-                  print(_enteredTitle);
+                  print(_titleController.text);
+                  print(_amountController.text);
                 } , 
                 child: const Text('Add expense') 
               )
