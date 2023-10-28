0e28271edb489846f7c148a8fbbf0ac1d417b0c5
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-10-28 13:43:03 +0530

    created basic modal sheet!

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,10 +1,11 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
-import 'package:expense_tracker/widgets/expenses.dart';
+// import 'package:expense_tracker/widgets/expenses.dart';
 
 class Expenses extends StatefulWidget{
   const Expenses({super.key});
+  @override
   State<Expenses> createState(){
     return _ExpensesState();
   }
@@ -25,12 +26,23 @@ class _ExpensesState extends State<Expenses>{
       category: Category.leisure,
     ),
   ];
+
+  void _openAddExpenseOverlay(){
+    showModalBottomSheet(context: context, builder: (ctx)=>
+      const Text('Modal sheet'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text('Trackr'),
         actions: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.add)) ,
+          IconButton(
+            onPressed: _openAddExpenseOverlay, 
+            icon: const Icon(Icons.add)
+          ) ,
         ],
       ),
       body: Column(
