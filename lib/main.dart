a544e6d6f16f20c652a94189f2445c49deb35d96
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-11-11 20:24:28 +0530

    added dark scheme!

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
 var kColorScheme = ColorScheme.fromSeed(seedColor:const  Color.fromARGB(255, 96, 59, 181));
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor:const  Color.fromARGB(255, 5, 89, 125));
 
 void main() {
   runApp(
      MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        cardTheme:const CardTheme().copyWith(
+            color: kDarkColorScheme.secondaryContainer,
+            margin:const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+          ),
+      ),
       theme: ThemeData().copyWith(
         useMaterial3: true,
         // scaffoldBackgroundColor: const Color.fromARGB(255, 70, 28, 115),
@@ -29,6 +43,7 @@ void main() {
             ),
           )
         ),
+        themeMode: ThemeMode.dark,
       home:const Expenses() ,
     )
   );
