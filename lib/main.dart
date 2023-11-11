5c7b884a4303cf6381e6a3f65b3600674f8df989
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-11-11 15:08:22 +0530

    theming started!

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:expense_tracker/widgets/expenses.dart';
 void main() {
   runApp(
      MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(useMaterial3: true,scaffoldBackgroundColor: Color.fromARGB(255, 70, 28, 115)),
       home:const Expenses() ,
     )
   );
