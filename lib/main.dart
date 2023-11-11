994b347bf62bbe2a5927d69e5471fb4e7545f929
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-11-11 19:56:54 +0530

    understading theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(seedColor:const  Color.fromARGB(255, 96, 59, 181));
+
 void main() {
   runApp(
      MaterialApp(
-      theme: ThemeData().copyWith(useMaterial3: true,scaffoldBackgroundColor: Color.fromARGB(255, 70, 28, 115)),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        // scaffoldBackgroundColor: const Color.fromARGB(255, 70, 28, 115),
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+          ),
+          cardTheme:const CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin:const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+            titleLarge: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: kColorScheme.onSecondaryContainer,
+              fontSize: 25,
+            ),
+          )
+        ),
       home:const Expenses() ,
     )
   );
