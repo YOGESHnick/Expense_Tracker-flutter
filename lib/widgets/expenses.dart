07563de4a62fdb6c7f71534500db3a26d3dd1cec
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-11-11 14:09:57 +0530

    new feature!

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -30,6 +30,7 @@ class _ExpensesState extends State<Expenses>{
 
   void _openAddExpenseOverlay(){
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context, 
       builder: (ctx) => NewExpense(onAddExpense:_addExpense),
     );
@@ -37,6 +38,12 @@ class _ExpensesState extends State<Expenses>{
   void _addExpense(Expense expense){
     setState(() {
       _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense){
+    setState(() {
+      _registeredExpenses.remove(expense);
     });
   }
 
@@ -56,7 +63,9 @@ class _ExpensesState extends State<Expenses>{
         children:  [
           const Text('The chart'),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses)
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+              onRemoveExpense:_removeExpense )
           ),
         ],
       ),
